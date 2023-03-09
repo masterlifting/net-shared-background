5f7469bf6ef5fb59a1fb36381fed7bd0a5ea0092
Andrey Pestunov <andreypestunov@gmail.com>
2023-03-09 02:06:36 +0100

    chore: recover code base

--- src/Net.Shared.Background.Models/Settings/ProcessStepsSettings.cs
+++ src/Net.Shared.Background.Models/Settings/ProcessStepsSettings.cs
@@ -2,7 +2,7 @@
 
 public sealed record ProcessStepsSettings
 {
-    public int ProcessingMaxCount { get; init; }
+    public int ProcessingMaxCount { get; set; }
     public bool IsParallelProcessing { get; init; }
     public string[] Names { get; init; } = Array.Empty<string>();
 }
